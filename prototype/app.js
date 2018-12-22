d6d4e5a11fc1d490dae5ca8d0dc0db854956c064
IB-2015 <1522411@stud.hs-mannheim.de>
2018-12-22 15:44:14 +0100

    prototype reading csv file

--- prototype/app.js
+++ prototype/app.js
@@ -1,5 +1,11 @@
 var express = require('express');
+var fs = require("fs");
+var d3 = require("d3");
+var countries = [];
+var d3_countries;
 var app = express();
+
+
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', function (req, res) {
@@ -8,4 +14,12 @@ app.get('/', function (req, res) {
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
+});
+
+fs.readFile("resources/geoscheme.csv", "utf8", function(error, data) {
+  d3_countries = d3.csvParse(data);
+  d3_countries.forEach(function(d) {
+    countries.push(d);
+    console.log(countries.length + '.) ' + d['Country or Area']);
+  })
 });
